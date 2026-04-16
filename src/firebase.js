86b49e5b8ead040f1df5dc776e90b6209f7c044b
Deyvid Dimitrov <davida1991@gmail.com>
2026-04-16 16:12:58 +0300

    security: move firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNTbG7gTCRNGP9_fF4Rohl7vR83mKL5ug",
-  authDomain: "pokanipro.firebaseapp.com",
-  projectId: "pokanipro",
-  storageBucket: "pokanipro.firebasestorage.app",
-  messagingSenderId: "1043317640536",
-  appId: "1:1043317640536:web:45d6cf9cc1c7ce104e692d",
-  measurementId: "G-LT64J969Z2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
